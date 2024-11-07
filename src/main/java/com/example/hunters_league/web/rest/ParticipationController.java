383a1b1d20148f4e3743003b9ae2677f2fd32e4e
zakaria <kharroubzakaria32@gmail.com>
2024-11-07 21:32:35 +0100

    participation fix fr competition and handle exception

--- src/main/java/com/example/hunters_league/web/rest/ParticipationController.java
+++ src/main/java/com/example/hunters_league/web/rest/ParticipationController.java
@@ -1,10 +1,13 @@
 package com.example.hunters_league.web.rest;
 
+import com.example.hunters_league.domain.Competition;
 import com.example.hunters_league.domain.Participation;
 import com.example.hunters_league.domain.User;
+import com.example.hunters_league.service.CompetitionService;
 import com.example.hunters_league.service.ParticipationService;
 import com.example.hunters_league.service.UserService;
 import com.example.hunters_league.service.dto.ParticipationDTO;
+import com.example.hunters_league.web.errors.competition.CompetitionNotFoundException;
 import com.example.hunters_league.web.errors.user.UserNotFoundException;
 import com.example.hunters_league.web.vm.ParticipationVM;
 import com.example.hunters_league.web.vm.mapper.ParticipationMapper;
@@ -20,11 +23,14 @@ public class ParticipationController {
     private final ParticipationService participationService;
     private final ParticipationMapper participationMapper;
     private final UserService userService;
+    private final CompetitionService competitionService;
 
-    public ParticipationController(ParticipationService participationService, ParticipationMapper participationMapper,UserService userService) {
+
+    public ParticipationController(ParticipationService participationService, ParticipationMapper participationMapper, UserService userService, CompetitionService competitionService) {
         this.participationService = participationService;
         this.participationMapper = participationMapper;
         this.userService = userService;
+        this.competitionService = competitionService;
     }
 
     @GetMapping("/find/{id}")
@@ -34,12 +40,19 @@ public class ParticipationController {
         return ResponseEntity.ok(participationDTO);
     }
 
+
     @PostMapping("/save")
     public ResponseEntity<?> save(@RequestBody ParticipationVM participationVM) {
         try {
             User user = userService.findById(participationVM.getUserId().toString());
         } catch (UserNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        try {
+            Competition competition = competitionService.findById(participationVM.getCompetitionId().toString());
+        } catch (CompetitionNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Competition not found");
         }
 
         Participation participation = participationMapper.toEntity(participationVM);
