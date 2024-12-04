33cd18d6e396e37c276290c5d9db083effdfa940
zakaria <kharroubzakaria32@gmail.com>
2024-12-04 12:01:05 +0100

    setup role and permissions

--- src/main/java/com/example/hunters_league/domain/AppUser.java
+++ src/main/java/com/example/hunters_league/domain/AppUser.java
@@ -13,8 +13,6 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -63,6 +61,94 @@ public class AppUser implements UserDetails {
     @Override
     public String getUsername() {
         return email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public LocalDateTime getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(LocalDateTime joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public LocalDateTime getLicenseExpirationDate() {
+        return licenseExpirationDate;
+    }
+
+    public void setLicenseExpirationDate(LocalDateTime licenseExpirationDate) {
+        this.licenseExpirationDate = licenseExpirationDate;
+    }
+
+    public List<Participation> getParticipations() {
+        return participations;
+    }
+
+    public void setParticipations(List<Participation> participations) {
+        this.participations = participations;
     }
 
     @Override
--- src/main/java/com/example/hunters_league/domain/enums/Role.java
+++ src/main/java/com/example/hunters_league/domain/enums/Role.java
@@ -1,5 +1,39 @@
 package com.example.hunters_league.domain.enums;
 
+import java.util.Set;
+
 public enum Role {
-    ADMIN, MEMBER, JURY
+    ADMIN(Set.of(
+            Permission.CAN_PARTICIPATE,
+            Permission.CAN_VIEW_RANKINGS,
+            Permission.CAN_VIEW_COMPETITIONS
+    )),
+    MEMBER(Set.of(
+            Permission.CAN_PARTICIPATE,
+            Permission.CAN_VIEW_RANKINGS,
+            Permission.CAN_VIEW_COMPETITIONS,
+            Permission.CAN_STORE
+    )),
+    JURY(Set.of(
+            Permission.CAN_PARTICIPATE,
+            Permission.CAN_VIEW_RANKINGS,
+            Permission.CAN_VIEW_COMPETITIONS,
+            Permission.CAN_STORE,
+            Permission.CAN_MANAGE_COMPETITIONS,
+            Permission.CAN_MANAGE_USERS,
+            Permission.CAN_MANAGE_SPECIES,
+            Permission.CAN_MANAGE_SETTINGS
+    ));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions){
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions(){
+        return permissions;
+    }
+
+
 }
