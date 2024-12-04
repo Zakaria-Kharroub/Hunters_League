1f8daccd5cd0caea913e4941c8b03c45a1512b8b
zakaria <kharroubzakaria32@gmail.com>
2024-12-04 16:44:14 +0100

    first autority autority for admin

--- src/main/java/com/example/hunters_league/domain/enums/Role.java
+++ src/main/java/com/example/hunters_league/domain/enums/Role.java
@@ -3,18 +3,18 @@ package com.example.hunters_league.domain.enums;
 import java.util.Set;
 
 public enum Role {
-    ADMIN(Set.of(
+    MEMBER(Set.of(
             Permission.CAN_PARTICIPATE,
             Permission.CAN_VIEW_RANKINGS,
             Permission.CAN_VIEW_COMPETITIONS
     )),
-    MEMBER(Set.of(
+    JURY(Set.of(
             Permission.CAN_PARTICIPATE,
             Permission.CAN_VIEW_RANKINGS,
             Permission.CAN_VIEW_COMPETITIONS,
             Permission.CAN_STORE
     )),
-    JURY(Set.of(
+    ADMIN(Set.of(
             Permission.CAN_PARTICIPATE,
             Permission.CAN_VIEW_RANKINGS,
             Permission.CAN_VIEW_COMPETITIONS,
@@ -27,13 +27,11 @@ public enum Role {
 
     private final Set<Permission> permissions;
 
-    Role(Set<Permission> permissions){
+    Role(Set<Permission> permissions) {
         this.permissions = permissions;
     }
 
-    public Set<Permission> getPermissions(){
+    public Set<Permission> getPermissions() {
         return permissions;
     }
-
-
 }
--- src/main/java/com/example/hunters_league/web/rest/SpeciesController.java
+++ src/main/java/com/example/hunters_league/web/rest/SpeciesController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -42,6 +43,7 @@ public class SpeciesController {
         return new ResponseEntity<>(speciesDTO, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/")
     public ResponseEntity<List<SpeciesDTO>> findAll() {
         List<Species> speciesList = speciesService.findAll();
